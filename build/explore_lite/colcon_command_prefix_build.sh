1ed35c8a099bfbfb0092976a84fd03852f875a80
githubUbuntu <080bei034.safal@pcampus.edu.np>
2026-07-01 19:11:28 +0545

    Added

---
+++ build/explore_lite/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/safal/Desktop/unitreego2nav/install/explore_lite_msgs/share/explore_lite_msgs/package.sh"
---
+++ build/explore_lite_msgs/explore_lite_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/safal/Desktop/unitreego2nav/build/explore_lite_msgs/explore_lite_msgs__py
+# Build directory: /home/safal/Desktop/unitreego2nav/build/explore_lite_msgs/explore_lite_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
