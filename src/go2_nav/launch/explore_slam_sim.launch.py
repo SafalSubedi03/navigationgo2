728393387718b2b0f8eea306dfed7210e915896c
githubUbuntu <080bei034.safal@pcampus.edu.np>
2026-07-01 18:05:10 +0545

    Topic remapping

--- src/go2_nav/launch/explore_slam_sim.launch.py
+++ src/go2_nav/launch/explore_slam_sim.launch.py
@@ -16,13 +16,12 @@
 
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import TimerAction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction, GroupAction
 
 def generate_launch_description():
 
@@ -61,15 +60,18 @@ def generate_launch_description():
     # -------------------------------------------------------------------
     nav2_bringup_share = get_package_share_directory('nav2_bringup')
 
-    navigation_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(nav2_bringup_share, 'launch', 'navigation_launch.py')
+    navigation_launch = GroupAction([
+        SetRemap(src='/cmd_vel', dst='/cmd_vel_manual'),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(nav2_bringup_share, 'launch', 'navigation_launch.py')
+            ),
+            launch_arguments={
+                'use_sim_time': use_sim_time,
+                'params_file':  nav2_params_path,
+            }.items(),
         ),
-        launch_arguments={
-            'use_sim_time': use_sim_time,
-            'params_file':  nav2_params_path,
-        }.items(),
-    )
+    ])
 
     # -------------------------------------------------------------------
     # 3. explore_lite -- frontier selection, sends NavigateToPose goals
