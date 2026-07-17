bb18220d0160d45042ec1c96b01783edb6b46363
githubUbuntu <080bei034.safal@pcampus.edu.np>
2026-07-17 12:32:26 +0545

    Added camera and lidar tf

--- src/go2camerainfo/launch/camera_info.launch.py
+++ src/go2camerainfo/launch/camera_info.launch.py
@@ -14,5 +14,24 @@ def generate_launch_description():
                 'publish_rate_hz': 30.0,
                 # 'calib_path': '/custom/path/if/you/want/to/override.json',
             }]
+        ),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='camera_tf_broadcaster',
+            arguments=['--x', '0.28', '--y', '0', '--z', '0.05',
+                       '--roll', '0', '--pitch', '0', '--yaw', '0',
+                       '--frame-id', 'base_link',
+                       '--child-frame-id', 'camera_link'],
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='lidar_tf_broadcaster',
+            arguments=['--x', '0.28945', '--y', '0', '--z', '-0.046825',
+                       '--roll', '0', '--pitch', '2.8782', '--yaw', '0',
+                       '--frame-id', 'base_link',
+                       '--child-frame-id', '<REAL_LIDAR_FRAME>'],  # fill this in
         )
     ])
